689ff93e6770e12802dd006d74fb259efc973e92
Gourav <gourav@g.com>
2023-06-15 08:35:04 +0530

    page classes

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,102 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+	
+	WebDriver driver;
+	@FindBy(xpath = "//*[@class='a-carousel-card']//img[@alt='Summer appliances']")
+	private WebElement summerdealcarousel;
+	
+	@FindBy(xpath = "//*[@id='nav-cart']")
+	private WebElement carticon;
+	
+	@FindBy(xpath = "//*[@id='nav-link-accountList']")
+	private WebElement accountsignin;
+	
+	@FindBy(xpath = "//*[@id='nav-flyout-ya-signin']//*[@class='nav-action-signin-button']")
+	private WebElement signinhoverbutton;
+	
+	@FindBy(xpath = "//*[@id='ap_email']")
+	private WebElement emailfield;
+	
+	@FindBy(xpath = "//input[@id='continue']")
+	private WebElement continuebutton;
+	
+	@FindBy(xpath = "//input[@id='ap_password']")
+	private WebElement passwordfield;
+	
+	@FindBy(xpath = "//input[@id='signInSubmit']")
+	private WebElement finalsigninbutton;
+	
+	WebDriverWait wait;
+	
+	
+	public HomePage(WebDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+		
+	}
+	
+		
+	public String getTitleOfPage()
+	{
+		String title = driver.getTitle();
+		
+		return title;
+	}
+	
+	public boolean verifyCart()
+	{
+		boolean isdisplay = carticon.isDisplayed();
+		
+		return isdisplay;
+	}
+	
+	
+	public void goToSignPage()
+	{
+		Actions act = new Actions(driver);
+		
+		act.moveToElement(accountsignin).perform();
+		
+		signinhoverbutton.click();
+	}
+	
+	public void enterUserName(String uname)
+	{
+		emailfield.sendKeys(uname);
+		continuebutton.click();
+	}
+	
+	public void finalSignIn(String pwd)
+	{
+		passwordfield.sendKeys(pwd);
+		finalsigninbutton.click();
+		
+	}
+	
+	
+	public void enterToSummerDeals()
+	{
+		
+		wait.until(ExpectedConditions.visibilityOf(summerdealcarousel));
+		summerdealcarousel.click();
+		
+	}
+	
+	
+	
+	
+
+}
--- src/test/resources/appfeatures/HomePage.feature
+++ src/test/resources/appfeatures/HomePage.feature
@@ -8,5 +8,15 @@ Given User is at the landing page
 Then Cart icon should get display
 
 Scenario: Checkout the deals section
+Given User is at the landing page
+When User click on summer section deal
+
+Scenario: Login to app
+Given User is at the landing page
+When User click on signin button
+And User enters username
+And User enters password with Signin
+Then User should get logged into app
+
 
 
